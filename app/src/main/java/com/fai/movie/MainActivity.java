8988e5c38bf98fbf7b32dad0d74724f91fcc04e2
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2017-05-22 23:09:20 +0700

    add default load and fix minor bug

--- app/src/main/java/com/fai/movie/MainActivity.java
+++ app/src/main/java/com/fai/movie/MainActivity.java
@@ -70,6 +70,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        requestJsonObject(0);
+
         // Sliding Drawer
         mNavItems.add(new NavItem("Setting", "Setting Application", R.drawable.icon));
         mNavItems.add(new NavItem("About", "", R.drawable.icon));
@@ -239,17 +242,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplication(), "Error Response", Toast.LENGTH_SHORT).show();
             }
         });
-//         If Need Header Response
-//        }) {
-//            @Override
-//            public Map<String, String> getHeaders() throws AuthFailureError {
-//                HashMap<String, String> test = new HashMap<String, String>();
-//                test.put("X-Mashape-Key","C0BHO4cIsBmshBlylonaV0Vhb28lp1kKh2xjsn3JP2hFHaO7Dm");
-//                test.put("Accept","text/plain");
-//                return test;
-////                return super.getHeaders();
-//            }
-//        };
         queue.add(stringRequest);
     }
 }
